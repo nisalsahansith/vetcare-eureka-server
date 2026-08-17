3331eadcd275b6892576d8e3ed2d23ba977b5e5d
nisalsahansith <nisalsahansith@gmail.com>
2026-08-17 17:00:40 +0530

    create eureka discovery service

---
+++ src/test/java/com/vetcare/discovery/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vetcare.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
